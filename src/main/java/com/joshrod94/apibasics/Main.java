27eb9624eb530eaee3ed5ff499f2ef3e4568dbb7
joshrod94 <47401286+joshrod94@users.noreply.github.com>
2026-01-05 19:27:30 -0500

    Send HTTP request and print API response

--- src/main/java/com/joshrod94/apibasics/Main.java
+++ src/main/java/com/joshrod94/apibasics/Main.java
@@ -3,10 +3,12 @@ package com.joshrod94.apibasics;
 import java.net.http.HttpClient;
 import java.net.URI;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         HttpClient client = HttpClient.newHttpClient();
 
@@ -14,12 +16,21 @@ public class Main {
                 .uri(URI.create("https://api.github.com"))
                 .GET()
                 .build();
+
+        HttpResponse<String> response = client.send(
+                request,
+                HttpResponse.BodyHandlers.ofString()
+        );
+
         // Step 1: Create an HTTP client
         // Step 2: Create an HTTP request
         // Step 3: Send the request
         // Step 4: Receive the response
         // Step 5: Print the response
 
-        System.out.println("Program started");
+        System.out.println("Status code: " + response.statusCode());
+        System.out.println("Response body:");
+        System.out.println(response.body());
+
     }
 }
