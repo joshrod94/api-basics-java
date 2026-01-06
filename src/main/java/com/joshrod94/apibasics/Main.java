e479a8d77884c5e181b8d448f999480856099b38
joshrod94 <47401286+joshrod94@users.noreply.github.com>
2026-01-05 19:23:16 -0500

    Create HttpRequest for GitHub API

--- src/main/java/com/joshrod94/apibasics/Main.java
+++ src/main/java/com/joshrod94/apibasics/Main.java
@@ -1,12 +1,19 @@
 package com.joshrod94.apibasics;
 
 import java.net.http.HttpClient;
+import java.net.URI;
+import java.net.http.HttpRequest;
+
 
 public class Main {
     public static void main(String[] args) {
 
         HttpClient client = HttpClient.newHttpClient();
 
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.github.com"))
+                .GET()
+                .build();
         // Step 1: Create an HTTP client
         // Step 2: Create an HTTP request
         // Step 3: Send the request
